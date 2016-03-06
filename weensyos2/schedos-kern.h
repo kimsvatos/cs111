a52e1004e4b51e2a8e6fff6596d521f1eaf07b88
kimsvatos <kimsvatos@ucla.edu>
2016-03-06 11:16:00 -0800

    exercise 4a

--- weensyos2/schedos-kern.h
+++ weensyos2/schedos-kern.h
@@ -23,7 +23,10 @@ typedef enum procstate {
 // Process descriptor type
 typedef struct process {
 	pid_t p_pid;			// Process ID
-
+	unsigned int p_priority;
+	unsigned int p_share;
+	unsigned int p_run_t;
+	
 	registers_t p_registers;	// Current process state: registers,
 					// stack location, EIP, etc.
 					// 'registers_t' defined in x86.h
